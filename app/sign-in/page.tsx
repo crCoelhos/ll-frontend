1477325445a5a5cc9d831dbdce19449994969b9b
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2023-11-28 22:22:39 -0500

    chore(navigate) - working

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -9,6 +9,8 @@ import { Button } from "@/components/ui/button";
 import { Icons } from "../_components/icons";
 
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import LoginBox from "../_components/loginBox";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> { }
 
@@ -17,6 +19,7 @@ export default function UserAuthForm({
   ...props
 }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
 
   async function onSubmit(e: React.SyntheticEvent) {
     e.preventDefault();
@@ -40,7 +43,6 @@ export default function UserAuthForm({
         }
       );
       sessionStorage.setItem("user_key", response.data.token);
-
     } catch (error) {
       console.error(error);
     } finally {
@@ -49,67 +51,9 @@ export default function UserAuthForm({
   }
 
   return (
-    <div className="w-[512px] mt-[248px] mx-[512px]">
-      <div className={cn("grid gap-6", className)} {...props}>
-        <form onSubmit={onSubmit}>
-          <div className="grid gap-2">
-            <div className="grid gap-1">
-              <Label className="sr-only" htmlFor="email">
-                Email
-              </Label>
-              <Input
-                id="email"
-                placeholder="jorge@example.com"
-                type="email"
-                name="email"
-                autoCapitalize="none"
-                autoComplete="email"
-                autoCorrect="off"
-                disabled={isLoading}
-              />
-            </div>
-            <div className="grid gap-1">
-              <Label className="sr-only" htmlFor="password">
-                Senha
-              </Label>
-              <Input
-                id="password"
-                placeholder="Senha"
-                name="password"
-                type="password"
-                autoCapitalize="none"
-                autoComplete="off"
-                autoCorrect="off"
-                disabled={isLoading}
-              />
-            </div>
-            <Button disabled={isLoading}>
-              {isLoading && (
-                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-              )}
-              Entrar
-            </Button>
-          </div>
-        </form>
-        <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <span className="w-full border-t" />
-          </div>
-          <div className="relative flex justify-center text-xs uppercase">
-            <span className="bg-background px-2 text-muted-foreground">
-              Ou acesse com
-            </span>
-          </div>
-        </div>
-        <Button variant="outline" type="button" disabled={isLoading}>
-          {isLoading ? (
-            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <Icons.google className="mr-2 h-4 w-4" />
-          )}
-          Google
-        </Button>
-      </div>
+
+    <div>
+      <LoginBox />
     </div>
   );
 }
