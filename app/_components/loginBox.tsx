263f71cb63844dde5a6263ba05bcfa22d39feea2
Christopher Ric Coelho Saar <cristophersaar@gmail.com>
2024-02-27 16:31:55 -0500

    Add console log for API URL

--- app/_components/loginBox.tsx
+++ app/_components/loginBox.tsx
@@ -29,6 +29,7 @@ export default function LoginBox({ className, ...props }: LoginBoxProps) {
     });
 
     try {
+      console.log("URL da API: ", process.env.NEXT_PUBLIC_API_URL);
       const response = await axios.post(
         process.env.NEXT_PUBLIC_API_URL + "signin",
         formDataObject,
